8c68e9f2d7620a2a311400f9bfbb0fe83a9c605f
Gele Hasan <zeinababubakar@Zeinabs-MacBook.local>
2023-06-16 02:32:34 +0200

    Testing http request using chai library

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app= express();
+
+app.get('/',(req, res)=>{
+
+    res.send('Main page')
+})
+
+app.listen(3000, ()=>{
+
+    console.log('Port has started')
+})
--- test/test.js
+++ test/test.js
@@ -1,11 +1,51 @@
 //var assert = require('assert');
 import assert from 'assert';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
 describe('Tests functions', ()=>{
     let word='Space unit';
     let wordSplit= word.split(' ');
     describe('Test length', ()=>{
         it('It should be equal to two', ()=>{
             assert.equal(wordSplit.length, 2);
+        })
+    })
+    
+    describe('Test http request', ()=>{
+
+        it('it should return 200', (done)=>{
+        
+            chai.request('http://localhost:3000')
+            .get('/')
+            .end((err,res)=>{
+            
+                assert.equal(res.status, 200);
+
+                done();
+            }
+            
+            )
+        })
+    })
+
+
+    //this will fail
+    describe('Test unavaibble request', ()=>{
+
+        it('it should return 200', (done)=>{
+        
+            chai.request('http://localhost:3000')
+            .get('/about')
+            .end((err,res)=>{
+            
+                assert.equal(res.status, 200);
+
+                done();
+            }
+            
+            )
         })
     })
 })

5476e907630ec0e515a660cfe390fa5eac6c7d85
Gele Hasan <gellehussen@gmail.com>
2023-07-29 21:13:02 +0200

    Added mock data for the emplooyes

---
+++ emplooyes.js
@@ -0,0 +1,38 @@
+
+let emplooyes = 
+   [
+      {
+         id: 1, 
+         name: "Samuel", 
+         job:"Front-end developer",
+         Company:"Google"
+      },
+      {
+        id: 2, 
+        name: "Gele", 
+        job:".Net developer",
+        Company:"Microsoft"
+     },{
+        id: 3, 
+        name: "Jamie", 
+        job:"Full-stack developer",
+        Company:"Freelancer"
+     },
+     {
+        id: 4, 
+        name: "Karin", 
+        job:"Front-end developer",
+        Company:"Google"
+     },
+     {
+       id: 5, 
+       name: "Neo", 
+       job:"QA",
+       Company:"Microsoft"
+    },{
+       id: 6, 
+       name: "Amanda", 
+       job:"Full-stack developer",
+       Company:"Freelancer"
+    }
+   ];

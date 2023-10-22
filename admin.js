b9286179ad2fcd87ec8914e2e406272595cb2e59
Gele <gellehussen@gmail.com>
2023-10-22 22:19:27 +0200

    defined set ups for admin file

---
+++ admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+
+const users = [
+    {
+        id: 1,
+        username: 'admin',
+        passwordHash: '$OXbN0NS6.rig6WShgn6hd2' 
+        
+    }
+];

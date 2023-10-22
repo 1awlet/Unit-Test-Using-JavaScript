5313363a6db186cb02daf9b80adfdb20c55fb08a
Gele <gellehussen@gmail.com>
2023-10-22 21:57:10 +0200

    code formatting and organizing and adding new items

--- emplooyes.js
+++ emplooyes.js
@@ -1,75 +1,73 @@
 import express from 'express';
 const router = express.Router();
-let emplooyes = 
+let emplooyes =
    [
       {
-         id: 1, 
-         name: "Samuel", 
-         job:"Front-end developer",
-         Company:"Google"
+         id: 1,
+         name: "Samuel",
+         job: "Front-end developer",
+         Company: "Google"
       },
       {
-        id: 2, 
-        name: "Gele", 
-        job:".Net developer",
-        Company:"Microsoft"
-     },{
-        id: 3, 
-        name: "Jamie", 
-        job:"Full-stack developer",
-        Company:"Freelancer"
-     },
-     {
-        id: 4, 
-        name: "Karin", 
-        job:"Front-end developer",
-        Company:"Google"
-     },
-     {
-       id: 5, 
-       name: "Neo", 
-       job:"QA",
-       Company:"Microsoft"
-    },{
-       id: 6, 
-       name: "Amanda", 
-       job:"Full-stack developer",
-       Company:"Freelancer"
-    }
+         id: 2,
+         name: "Gele",
+         job: ".Net developer",
+         Company: "Microsoft"
+      }, {
+         id: 3,
+         name: "Jamie",
+         job: "Full-stack developer",
+         Company: "Freelancer"
+      },
+      {
+         id: 4,
+         name: "Karin",
+         job: "Front-end developer",
+         Company: "Google"
+      },
+      {
+         id: 5,
+         name: "Neo",
+         job: "QA",
+         Company: "Microsoft"
+      }, {
+         id: 6,
+         name: "Amanda",
+         job: "Full-stack developer",
+         Company: "Freelancer"
+      }
    ];
 
 
-router.get('/', function(req, res){
-    res.json(emplooyes);
-  
+router.get('/', function (req, res) {
+   res.json(emplooyes);
+
 });
 
 
-router.get('/:id', function(req, res){
+router.get('/:id', function (req, res) {
 
-    const emplooyID = req.params['id'];
-    const chosenEmplooye = emplooyes.find((employee) => employee.id === parseInt(emplooyID));
- 
- 
-    if(spell){
-   
-       res.json(chosenEmplooye);
-    }else{
-        res.status(404).json({ error: 'Employee not founnd' });
-    }
-   
- });
+   const emplooyID = req.params['id'];
+   const chosenEmplooye = emplooyes.find((employee) => employee.id === parseInt(emplooyID));
 
- router.put('/:id', function(req, res){
+   if (spell) {
 
- })
+      res.json(chosenEmplooye);
+   } else {
+      res.status(404).json({ error: 'Employee not founnd' });
+   }
 
- router.post('/add', function(req, res){
+});
 
- })
+router.put('/:id', function (req, res) {
 
-module.exports={
-    router: router,
-    emplooyes:emplooyes
- } 
- 
+})
+
+router.post('/add', function (req, res) {
+
+})
+
+module.exports = {
+   router: router,
+   emplooyes: emplooyes
+}
--- test/test.js
+++ test/test.js
@@ -5,147 +5,144 @@ import chaiHttp from 'chai-http';
 
 const emplooyes = require("../emplooyes").emplooyes;
 chai.use(chaiHttp);
-describe('Tests functions', ()=>{
-    let word='Space unit';
-    let wordSplit= word.split(' ');
-    describe('Test length', ()=>{
-        it('It should be equal to two', ()=>{
+
+
+describe('Tests functions', () => {
+    let word = 'Space unit';
+    let wordSplit = word.split(' ');
+    describe('Test length', () => {
+        it('It should be equal to two', () => {
             assert.equal(wordSplit.length, 2);
         })
-    })
+})
+
     //WIll pass
-    describe('Test http request', ()=>{
-
-        it('it should return 200', (done)=>{
-        
-            chai.request('http://localhost:3000')
+describe('Test http request', () => {
+    it('it should return 200', (done) => {
+        chai.request('http://localhost:3000')
             .get('/')
-            .end((err,res)=>{
-            
+            .end((err, res) => {
                 assert.equal(res.status, 200);
 
                 done();
             }
-            
             )
-        })
-    })
-
-
-    //this will fail
-    describe('Test unavaibble request', ()=>{
-
-        it('it should return 200', (done)=>{
-        
-            chai.request('http://localhost:3000')
-            .get('/about')
-            .end((err,res)=>{
-            
-                assert.equal(res.status, 200);
-
-                done();
-            }
-            
-            )
-        })
     })
 })
 
-//users
-describe("Does the does user exist?", ()=>{
 
-    it("The users lists should exist", (done)=>{
+    //this will fail
+describe('Test unavaibble request', () => {
+
+    it('it should return 200', (done) => {
+
+        chai.request('http://localhost:3000')
+            .get('/about')
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+
+                done();
+            }
+
+            )
+    })
+})
+})
+
+//users
+describe("Does the does user exist?", () => {
+
+    it("The users lists should exist", (done) => {
 
         chai.request(port)
-        .get('/emplooyes')
-        .end((req,res)=>{   
-            assert.equal(res.status, 200)
-            done()
-        })
+            .get('/emplooyes')
+            .end((req, res) => {
+                assert.equal(res.status, 200)
+                done()
+            })
     })
 })
 
 //test to check the default route message
-describe("Check the output message", ()=>{
+describe("Check the output message", () => {
 
-    it("The message shown should be correct", (done)=>{
+    it("The message shown should be correct", (done) => {
         chai.request(port)
-        .get('/')
-        .end((req,res)=>{
-        let data = JSON.parse(res.text);
-        assert.equal(data.message, "Welcome to emplooyes dashboard");
-            done();
-        })
+            .get('/')
+            .end((req, res) => {
+                let data = JSON.parse(res.text);
+                assert.equal(data.message, "Welcome to emplooyes dashboard");
+                done();
+            })
     })
 })
 
-describe("It should send back the right data for specific emplooye", ()=>{
+describe("It should send back the right data for specific emplooye", () => {
     //The emplote we going to test
-     const emplooyeId = 1003;
-     it("The end point for specific emplooye should be reached", (done)=>{
-         chai.request(port)
-         .get(`/emplooyes/${emplooyeId}`)
-         .end((req,res)=>{   
-             assert.equal(res.status, 200);
-             done();
-         })
-     })
-
-     it("The response should return as form of an object ", (done)=>{
+    const emplooyeId = 1003;
+    it("The end point for specific emplooye should be reached", (done) => {
         chai.request(port)
-        .get(`/emplooyes/${emplooyeId}`)
-        .end((req,res)=>{
-            const resData = res.body; 
-             assert.equal(typeof resData, "object")
-            done();
-        })
+            .get(`/emplooyes/${emplooyeId}`)
+            .end((req, res) => {
+                assert.equal(res.status, 200);
+                done();
+            })
+    })
+
+    it("The response should return as form of an object ", (done) => {
+        chai.request(port)
+            .get(`/emplooyes/${emplooyeId}`)
+            .end((req, res) => {
+                const resData = res.body;
+                assert.equal(typeof resData, "object")
+                done();
+            })
     })
 
 
     it("Specific emplooye data shown in response should match the data in emplooye array", (done) => {
-            const expectedResult= emplooyes.find((spell)=>spell.id== emplooyeId); 
-            
-            chai.request('http://localhost:3000')
-              .get(`/emplooyes/${emplooyeId}`)
-              .end((err, res) => {
-                const responseData = JSON.parse(res.text); 
-              assert.deepEqual(JSON.stringify(responseData), JSON.stringify(expectedResult));
-               done();
-          
-              });
-          });
+        const expectedResult = emplooyes.find((spell) => spell.id == emplooyeId);
 
-    })
+        chai.request('http://localhost:3000')
+            .get(`/emplooyes/${emplooyeId}`)
+            .end((err, res) => {
+                const responseData = JSON.parse(res.text);
+                assert.deepEqual(JSON.stringify(responseData), JSON.stringify(expectedResult));
+                done();
 
-const addEmplooye= (id,name,job, company)=>{
+            });
+    });
 
-    if( typeof id !== "number",  typeof name !== "string", typeof job !== "string",
+})
+
+const addEmplooye = (id, name, job, company) => {
+
+    if (typeof id !== "number", typeof name !== "string", typeof job !== "string",
         typeof company !== "string") {
         return null
-        }
+    }
     return {
         id,
         name,
         job,
         company,
-        
+
     }
 }
 
 
 describe("It should add a new emplooye", () => {
- 
-    const newEmplooye = addEmplooye(7, "Gele", "IT-Engineer",
-              "noneofyourBusiness");
 
-    it("it should contain the right properties ", ()=>{
+    const newEmplooye = addEmplooye(7, "Gele", "IT-Engineer",
+        "noneofyourBusiness");
+
+    it("it should contain the right properties ", () => {
         assert.property(newEmplooye, "name");
         assert.property(newEmplooye, "job");
         assert.property(newEmplooye, "company");
     })
 
-
-    it("it should have correct length ", ()=>{
+    it("it should have correct length ", () => {
 
         assert.strictEqual(Object.keys(newEmplooye).length, 4);
 
@@ -153,90 +150,82 @@ describe("It should add a new emplooye", () => {
 
     it("The new emplooye added  should exist and match with the provided values", (done) => {
         chai.request(port)
-          .post('/emplooyes/add')
-          .send(newEmplooye)
-          .end((err, res) => {
-            assert.equal(res.status, 200);  
-            assert.deepEqual(res.body, newEmplooye); 
-            console.log(res.body);
-            done();
-          });
-      });
-      
+            .post('/emplooyes/add')
+            .send(newEmplooye)
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, newEmplooye);
+                console.log(res.body);
+                done();
+            });
+    });
 
 })
 
 
-const updateEmplooye = (emplooyeId,updatedFields=[])=>{
-
-   
-    let updatedEmplooye= {
+const updateEmplooye = (emplooyeId, updatedFields = []) => {
+    let updatedEmplooye = {
         id: emplooyeId,
     }
 
+    const isEmplooyeFound = emplooyes.find((employe) => employe.id === emplooyeId);
 
-const isEmplooyeFound= emplooyes.find((employe) => employe.id === emplooyeId);
+    if (!isEmplooyeFound) return null;
 
-if(!isEmplooyeFound) return null ;
+    if (updatedFields.hasOwnProperty("name") && typeof updatedFields.name === "string") {
+        updatedEmplooye.name = updatedFields.name
+    }
 
-if( updatedFields.hasOwnProperty("name") && typeof updatedFields.name === "string"){
-    updatedEmplooye.name=updatedFields.name 
+    if (updatedFields.hasOwnProperty("job") && updatedFields.job === "string") {
+        updatedEmplooye.job = updatedFields.job
+    }
+
+
+    if (updatedFields.hasOwnProperty("company") && typeof updatedFields.result === "company") {
+        updatedEmplooye.result = updatedFields.result
+    }
+
+
+    return updatedEmplooye;
 }
 
-if(updatedFields.hasOwnProperty("job") && updatedFields.job=== "string"){
-    updatedEmplooye.job=updatedFields.job
-} 
 
 
-if(updatedFields.hasOwnProperty("company") && typeof updatedFields.result === "company")  {
-    updatedEmplooye.result=updatedFields.result
-}
+describe("Update a specific spell", () => {
+    const emplooyeId = 1003;
+    const updatedEmplooye = updateEmplooye(emplooyeId, { name: "Samuel", job: "It support", company: "Moodys" });
+
+    it("should return null if the emplooye does not exist", () => {
+        let doesntExistId = 42332;
+        const testNonExistEmplooyee = updateEmplooye(doesntExistId);
+
+        assert.equal(testNonExistEmplooyee, null);
+    });
 
 
-return updatedEmplooye;
-}
-
-
-
-describe("Update a specific spell", ()=>{
-    const emplooyeId= 1003;
-
-const updatedEmplooye = updateEmplooye(emplooyeId,{name:"Samuel", job:"It support",  company:"Moodys"});
-
-it("should return null if the emplooye does not exist", () => {
-    let doesntExistId=42332;
-    const testNonExistEmplooyee = updateEmplooye(doesntExistId);
-
-    assert.equal(testNonExistEmplooyee, null);
-  });
-
-
-  it("The properties should have the right types", ()=>{
-    assert.equal(typeof updatedEmplooye.id, "number", "Id should be a number ");
-    assert.equal(typeof updatedEmplooye.name, "string", "name should be string");
-    assert.equal(typeof updatedEmplooye.job, "string"," job It should bea string");
-    assert.equal(typeof updatedSpell.company, "string", "company should be a string");
-  })
+    it("The properties should have the right types", () => {
+        assert.equal(typeof updatedEmplooye.id, "number", "Id should be a number ");
+        assert.equal(typeof updatedEmplooye.name, "string", "name should be string");
+        assert.equal(typeof updatedEmplooye.job, "string", " job It should bea string");
+        assert.equal(typeof updatedSpell.company, "string", "company should be a string");
+    })
 
 
 
 
-  it("it should update successfully ",  (done)=>{
+    it("it should update successfully ", (done) => {
 
-    chai.request(port)
-       .put(`/emplooyes/${emplooyeId}`)
-       .send(updatedEmplooye)
-       .end((req, res)=>{
-       
-           assert.equal(JSON.stringify(res.body), JSON.stringify(updatedEmplooye));
-        
-           done();
-       })
-    
-})
+        chai.request(port)
+            .put(`/emplooyes/${emplooyeId}`)
+            .send(updatedEmplooye)
+            .end((req, res) => {
 
+                assert.equal(JSON.stringify(res.body), JSON.stringify(updatedEmplooye));
 
+                done();
+            })
 
+    })
 
 
 })
